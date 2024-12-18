5bf233487f36669e1075c7cda976c24e62dd5c90
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-12-18 03:03:25 -0300

    feat: add Home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-[100vh]`}
       >
-        {children}
+        <div className="flex flex-col h-full">{children}</div>
       </body>
     </html>
   );
