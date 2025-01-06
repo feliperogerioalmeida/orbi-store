6bfd5c448e9c751b87567ecb96893fa03af6dcf8
Felipe Almeida <felipe.rogerio@outlook.com>
2025-01-06 00:13:37 -0300

    chore: organized the repository for LandingPage and LoginPage

--- app/(admin)/login/page.tsx
+++ app/(admin)/login/page.tsx
@@ -1,20 +1,14 @@
-import Image from "next/image";
-import { Input } from "./_components/ui/input";
-import { Button } from "./_components/ui/button";
-import { Card, CardContent } from "./_components/ui/card";
+import { Input } from "../../_components/ui/input";
+import { Button } from "../../_components/ui/button";
+import { Card, CardContent } from "../../_components/ui/card";
+import Logo from "@/app/_components/logo";
 
-export default function Home() {
+export default function LoginPage() {
   return (
     <div className="flex flex-col h-full w-full items-center justify-center gap-4">
       <Card className="flex flex-col items-center justify-center max-w-[400px] max-h-[400px] h-[80%] w-[90%]">
         <CardContent className="flex flex-col items-center gap-4">
-          <Image
-            src="/logo.svg"
-            alt="Orbi Store"
-            width={0}
-            height={0}
-            className="w-[80%] h-auto object-contain"
-          />
+          <Logo className="w-[80%]" />
           <Input type="email" placeholder="Email" className="w-[80%]" />
           <Input type="password" placeholder="Password" className="w-[80%]" />
           <Button className="w-[80%] text-sm uppercase font-bold hover:bg-white hover:text-primary hover:border-primary hover:border-solid hover:border-[1px]">
