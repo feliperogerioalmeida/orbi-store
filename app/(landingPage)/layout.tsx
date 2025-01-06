9b1d66d9c6fc44f5320c99473479d5f7b5ba44c4
Felipe Almeida <felipe.rogerio@outlook.com>
2025-01-06 19:39:24 -0300

    fix: ensure that prisma and nextAuth are not running to deploy only landingPage

--- app/(landingPage)/layout.tsx
+++ app/(landingPage)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import AuthProvider from "../_providers/auth";
+// import AuthProvider from "../_providers/auth";
 import LandingPageNav from "../_components/landingPageNav";
 import Footer from "../_components/footer";
 
@@ -31,14 +31,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex h-full flex-col">
-          <AuthProvider>
-            <div className="fixed z-10">
-              <LandingPageNav />
-            </div>
+          {/* <AuthProvider> */}
+          <div className="fixed z-10">
+            <LandingPageNav />
+          </div>
 
-            <div className="flex-1 ">{children}</div>
-            <Footer />
-          </AuthProvider>
+          <div className="flex-1 ">{children}</div>
+          <Footer />
+          {/* </AuthProvider> */}
         </div>
       </body>
     </html>
--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,9 +1,9 @@
-import { AuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { db } from "./prisma";
-import { Adapter } from "next-auth/adapters";
+// import { AuthOptions } from "next-auth";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { db } from "./prisma";
+// import { Adapter } from "next-auth/adapters";
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [],
-};
+// export const authOptions: AuthOptions = {
+//   adapter: PrismaAdapter(db) as Adapter,
+//   providers: [],
+// };
