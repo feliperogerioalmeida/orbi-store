20d6139194dc02003a1b81264bfd2040bd6de2d3
Felipe Almeida <felipe.rogerio@outlook.com>
2025-01-06 18:01:11 -0300

    feat: add constants infos

---
+++ app/constants.ts
@@ -0,0 +1,17 @@
+const ORBI_CONSTANTS = {
+  CONTACT: {
+    WHATSAPP: "+55 71 99973-2369",
+    INSTAGRAM: "@store.orbi",
+    EMAIL: "contato@orbi.com",
+  },
+  BUSINESS: {
+    CNPJ: "45.641.876/0001-55 ",
+    NAME: "Orbi Store Eletrônicos e Acessórios LTDA",
+  },
+  LINKS: {
+    WHATSAPP_URL: "https://wa.me/5571999732369",
+    INSTAGRAM_URL: "https://instagram.com/store.orbi",
+  },
+};
+
+export default ORBI_CONSTANTS;
