892140b23ff86182c8cd3d1bddd3f526b1d411c9
Felipe Almeida <felipe.rogerio@outlook.com>
2025-01-06 17:29:02 -0300

    style: changes on height and gaps to ensure the page has a better view

--- app/(landingPage)/(home)/components/homeSection.tsx
+++ app/(landingPage)/(home)/components/homeSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const HomeSection = () => {
   return (
-    <div className="items-center h-full min-h-[100vh] w-full bg-[url('/homeBg.jpg')] bg-cover bg-center">
+    <div className="items-center min-h-screen w-full bg-[url('/homeBg.jpg')] bg-cover bg-center">
       <div className="flex flex-col justify-center items-center h-[50%] ">
         <h1 className=" relative text-5xl font-extrabold text-center text-white text-shadow">
           <span className="absolute top-0 left-0 text-black -z-10 blur-sm">
@@ -34,7 +34,7 @@ const HomeSection = () => {
           <p className=" font-bold text-lg">Falar com um especialista</p>
         </Button>
       </div>
-      <div className="h-[50%] w-auto min-w-[100%] relative">
+      <div className="h-[50%] w-auto min-w-screem relative">
         <Image
           src="/homePerson.png"
           alt="homePerson"
--- app/(landingPage)/(home)/components/productsSection.tsx
+++ app/(landingPage)/(home)/components/productsSection.tsx
@@ -1,47 +1,49 @@
 import ProductSectionCard from "./productSectionCard";
 
 const ProductsSection = () => {
-  const productsinfo = [
+  const productsInfo = [
     {
       title: "iPhones Novos",
-      description: "Aparelhos lacrados com 1 ano de garantia da Apple",
+      description:
+        "Modelos lacrados, com 1 ano de garantia da Apple. Qualidade garantida para quem busca o melhor em tecnologia.",
       image: "/iphonesNovos.png",
     },
     {
       title: "iPhones Seminovos",
       description:
-        "aparelhos em excelentes condições, bateria acima de 80%, todas as peças originais e um preço mais acessível e com garantia de 3 meses",
+        "Dispositivos rigorosamente avaliados, com bateria acima de 80%, peças 100% originais e garantia de 3 meses. Perfeitos para quem busca qualidade a um preço acessível.",
       image: "/iphonesSeminovos.png",
     },
     {
-      title: "Acesssórios",
+      title: "Acessórios",
       description:
-        "Tudo que você precisa para o seu iPhone, iPad, Apple Watch. Produtos Originais e linha Premium",
+        "Carregadores, capas, películas e muito mais. Produtos originais e premium para complementar seu iPhone, iPad ou Apple Watch.",
       image: "/acessorios.png",
     },
     {
       title: "Caixas de Som e Alexa",
-      description: "Aparelhos lacrados com 1 ano de garantia da Apple",
+      description:
+        "Explore som de alta qualidade com as caixas JBL e torne sua casa mais inteligente com dispositivos Alexa. Produtos lacrados e com garantia de 1 ano.",
       image: "/jblAlexa.png",
     },
     {
-      title: "Produtos Apple e muito mais",
+      title: "Produtos Apple e Muito Mais",
       description:
-        "Apple Vision, Macbook, Apple Watch, Airpods, todos os produtos apple e muita mais.",
+        "De MacBooks a Apple Watch, passando por AirPods e o revolucionário Apple Vision. Encontre tudo da Apple e muito mais aqui.",
       image: "/produtosApple.png",
     },
   ];
 
   return (
-    <div className="flex flex-col items-center h-full min-h-screen w-full bg-[url('/productBg.png')] bg-cover bg-center bg-no-repeat pt-28 gap-4 pb-4">
-      <div className="flex">
+    <div className="flex flex-col items-center h-full min-h-screen w-full bg-[url('/productBg.png')] bg-cover bg-center bg-no-repeat ">
+      <div className="flex pt-36 ">
         <h2 className=" text-white text-5xl text-center">
           Conheça nossos produtos
         </h2>
       </div>
 
-      <div className="flex flex-col h-full min-h-screen justify-center items-center w-full gap-2 pb-8">
-        {productsinfo.map((product) => (
+      <div className="flex flex-col h-full min-h-screen justify-start items-center w-full gap-8 pt-4 ">
+        {productsInfo.map((product) => (
           <ProductSectionCard key={product.title} {...product} />
         ))}
       </div>
