a384593a2973cf41197e992b52c845f2675dbdc3
Felipe Almeida <felipe.rogerio@outlook.com>
2025-01-09 01:01:55 -0300

    feat: refactor authentication handling in login layout and API route

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import AuthProvider from "../_providers/auth";
-// import AuthProvider from "../_providers/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,17 +4,15 @@ import { redirect } from "next/navigation";
 import LoginForm from "./components/loginForm"; // Importa o Client Component
 
 const LoginPage = async () => {
-  // Obtém a sessão no lado do servidor
   const session = await getServerSession(authOptions);
 
-  // Se o usuário já estiver autenticado, redireciona para o dashboard
   if (session && session.user) {
     redirect("/dashboard");
   }
 
   return (
     <div className="flex flex-col h-full w-full items-center justify-center gap-4">
-      <LoginForm /> {/* Renderiza o Client Component */}
+      <LoginForm />
     </div>
   );
 };
