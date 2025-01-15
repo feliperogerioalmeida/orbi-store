51471b89a7f53b36bb629e99fa4b4dd32bc65d71
Felipe Almeida <felipe.rogerio@outlook.com>
2025-01-15 03:13:17 -0300

    feat: fetch and display dynamic user, simulation, and review counts on dashboard

--- app/(admin)/master/dashboard/page.tsx
+++ app/(admin)/master/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/_lib/auth";
 import { redirect } from "next/navigation";
+import { db } from "@/app/_lib/prisma";
 
 const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
@@ -13,19 +14,27 @@ const DashboardPage = async () => {
     redirect("/dashboard");
   }
 
+  const simulations = await db.simulation.findMany({
+    include: { user: true },
+  });
+
+  const users = await db.user.findMany();
+
+  const reviews = await db.review.findMany();
+
   return (
     <div className="grid grid-cols-2 w-full items-center justify-center gap-4 p-2">
       <div className="flex flex-col aspect-video bg-muted/50 w-full p-2 rounded-xl gap-1 h-min-[80px] h-[80px] justify-between">
         <p className="text-xs ">Usuários</p>
-        <p className="text-2xl font-bold">2</p>
+        <p className="text-2xl font-bold">{users.length}</p>
       </div>
-      <div className="flex flex-col aspect-videobg-muted/50 w-full p-2 rounded-xl gap-1 h-min-[80px] h-[80px] justify-between">
+      <div className="flex flex-col aspect-video bg-muted/50 w-full p-2 rounded-xl gap-1 h-min-[80px] h-[80px] justify-between">
         <p className="text-xs ">Simulações</p>
-        <p className="text-2xl font-bold">2</p>
+        <p className="text-2xl font-bold">{simulations?.length}</p>
       </div>
       <div className="flex flex-col aspect-video bg-muted/50 w-full p-2 rounded-xl gap-1 h-min-[80px] h-[80px] justify-between">
         <p className="text-xs ">Avaliação Média</p>
-        <p className="text-2xl font-bold">2</p>
+        <p className="text-2xl font-bold">{reviews?.length}</p>
       </div>
     </div>
   );
