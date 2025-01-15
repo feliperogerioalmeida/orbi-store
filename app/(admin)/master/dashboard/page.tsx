bfe834b75108659d8a4c99b9f4b08ab6f707d950
Felipe Almeida <felipe.rogerio@outlook.com>
2025-01-15 00:33:21 -0300

    feat: update dashboard card styles for improved visual consistency and responsiveness

--- app/(admin)/master/dashboard/page.tsx
+++ app/(admin)/master/dashboard/page.tsx
@@ -15,15 +15,15 @@ const DashboardPage = async () => {
 
   return (
     <div className="grid grid-cols-2 w-full items-center justify-center gap-4 p-2">
-      <div className="flex flex-col bg-accent w-full p-2 rounded-md gap-1 h-min-[80px] h-[80px] justify-between">
+      <div className="flex flex-col aspect-video bg-muted/50 w-full p-2 rounded-xl gap-1 h-min-[80px] h-[80px] justify-between">
         <p className="text-xs ">Usuários</p>
         <p className="text-2xl font-bold">2</p>
       </div>
-      <div className="flex flex-col bg-accent w-full p-2 rounded-md gap-1 h-min-[80px] h-[80px] justify-between">
+      <div className="flex flex-col aspect-videobg-muted/50 w-full p-2 rounded-xl gap-1 h-min-[80px] h-[80px] justify-between">
         <p className="text-xs ">Simulações</p>
         <p className="text-2xl font-bold">2</p>
       </div>
-      <div className="flex flex-col bg-accent w-full p-2 rounded-md gap-1 h-min-[80px] h-[80px] justify-between">
+      <div className="flex flex-col aspect-video bg-muted/50 w-full p-2 rounded-xl gap-1 h-min-[80px] h-[80px] justify-between">
         <p className="text-xs ">Avaliação Média</p>
         <p className="text-2xl font-bold">2</p>
       </div>
