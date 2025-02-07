5393ebfa37c8e6969fc2e8ba84aa995aa82cdfc1
Felipe Almeida <felipe.rogerio@outlook.com>
2025-02-07 12:51:39 -0300

    feat: add UserProvider and useUser hook for user context management

---
+++ app/_providers/user.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { Address, User } from "@prisma/client";
+import { getUser } from "../_actions/getUser";
+import { useSession } from "next-auth/react";
+
+interface ExtendedUser extends User {
+  address?: Address | null;
+}
+
+interface UserContextType {
+  user: ExtendedUser | null;
+  setUser: (user: User | null) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const { data: session } = useSession();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    // Atualize o estado do usuário com os dados da sessão
+    if (session?.user?.email) {
+      getUser(session.user.email).then(setUser);
+    }
+  }, [session]);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser deve ser usado dentro de UserProvider");
+  }
+  return context;
+};
