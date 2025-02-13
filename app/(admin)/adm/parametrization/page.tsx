879f0c02b1ca68fcff5f017a268797bf42e6d9f7
Felipe Almeida <felipe.rogerio@outlook.com>
2025-02-13 18:34:43 -0300

    feat: add additional tabs for banks, product categories, products, and expenses in ParametrizationPage

--- app/(admin)/adm/parametrization/page.tsx
+++ app/(admin)/adm/parametrization/page.tsx
@@ -9,7 +9,7 @@ import ChartOfAccountsTable from "./_components/chartOfAccountsTable";
 
 const ParametrizationPage = async () => {
   return (
-    <div className="flex flex-col justify-start p-4 gap-4 w-full h-full overscroll-none">
+    <div className="flex flex-col justify-start p-4 gap-4 w-full h-full overscroll-none ">
       <CustomSidebarTrigger content="Parametrização" />
       <div className="w-full flex flex-col justify-start gap-3 items-start">
         <Tabs defaultValue="chartOfAccounts" className=" w-full">
@@ -19,6 +19,21 @@ const ParametrizationPage = async () => {
               className="w-min-[100px] w-full"
             >
               Plano de contas
+            </TabsTrigger>
+            <TabsTrigger value="banks" className="w-min-[100px] w-full">
+              Bancos
+            </TabsTrigger>
+            <TabsTrigger
+              value="productCategories"
+              className="w-min-[100px] w-full"
+            >
+              Categoria
+            </TabsTrigger>
+            <TabsTrigger value="products" className="w-min-[100px] w-full">
+              Produtos
+            </TabsTrigger>
+            <TabsTrigger value="expenses" className="w-min-[100px] w-full">
+              Despesas
             </TabsTrigger>
           </TabsList>
           <TabsContent value="chartOfAccounts">
