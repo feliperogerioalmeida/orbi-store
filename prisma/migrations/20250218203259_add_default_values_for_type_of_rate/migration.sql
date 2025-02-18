6507f4c878a70b89c316a3661f592d0d87bac548
Felipe Almeida <felipe.rogerio@outlook.com>
2025-02-18 17:34:30 -0300

    feat: set default value for typeOfRate in PaymentMethod and ReceivingMethod to 'CASH'

---
+++ prisma/migrations/20250218203259_add_default_values_for_type_of_rate/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "PaymentMethod" ALTER COLUMN "typeOfRate" SET DEFAULT 'CASH';
+
+-- AlterTable
+ALTER TABLE "ReceivingMethod" ALTER COLUMN "typeOfRate" SET DEFAULT 'CASH';
